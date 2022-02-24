cc083eeb7c46e1bce2d8df200420544e090d0b44
davidolzbut <david.olzbut@sps-prosek.cz>
2022-02-24 15:37:22 +0100

    pridani GUI

---
+++ tkinter_demo.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.geometry('250x250')
+window.title('Applikace v1.1 alfa&omega')
+
+label = tk.Label(text="Zdar párci!")
+label.pack()
+
+button = tk.Button()
+
+window.mainloop()
