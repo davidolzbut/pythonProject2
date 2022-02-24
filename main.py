c2d7fcf92998bcc32f1f0395f10a214fd5f15182
davidolzbut <david.olzbut@sps-prosek.cz>
2022-02-24 15:19:19 +0100

    zmena 1, 2, 3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # This is a sample Python script.
-# zmena 1, 2
+# zmena 1, 2, 3
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
